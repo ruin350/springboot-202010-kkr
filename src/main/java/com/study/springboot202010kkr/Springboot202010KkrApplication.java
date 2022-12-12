89cd566456808669e13b346b7701266d87b46485
ruin350 <kkr583@naver.com>
2022-12-12 15:31:29 +0900

    기본

---
+++ src/main/java/com/study/springboot202010kkr/Springboot202010KkrApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot202010kkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot202010KkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot202010KkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/springboot202010kkr/Springboot202010KkrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot202010kkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot202010KkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
