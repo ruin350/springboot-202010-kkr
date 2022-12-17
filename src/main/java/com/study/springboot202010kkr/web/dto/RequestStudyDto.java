21f1402ba64fcf9c3c539a8358c0a2d9c6e243b6
ruin350 <kkr583@naver.com>
2022-12-17 15:49:57 +0900

    복습 ResponseEntity

---
+++ src/main/java/com/study/springboot202010kkr/web/dto/RequestStudyDto.java
@@ -0,0 +1,13 @@
+package com.study.springboot202010kkr.web.dto;
+
+
+import lombok.Data;
+
+
+
+@Data
+public class RequestStudyDto {
+    private String name;
+    private String email;
+    private String address;
+}
