4a8bdba3729a5fe0ad11fda239ec5ca2f48fff56
ruin350 <kkr583@naver.com>
2023-01-11 13:31:05 +0900

    정규식

--- src/main/java/com/study/springboot202010kkr/web/dto/UserDto.java
+++ src/main/java/com/study/springboot202010kkr/web/dto/UserDto.java
@@ -5,14 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class UserDto {
     private int userId;
+    @Pattern(regexp = "^[a-zA-Z0-9]{5,20}$", message = "사용자이름은 영문, 숫자 조합이어야하며 5자 이상 20자 이하로작성하세요")
     private String username;
     private String password;
+    @Pattern(regexp = "^[가-힇]{2,5}$", message = "이름은 한글만 작성 가능하며 최대 5자까지만 입력가능합니다")
     private String name;
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email
     private String email;
 }
